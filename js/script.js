8083488719ac81cab7d7f8412c51ae8d12044458
Hanan-Hussein <hanhuss035@gmail.com>
2022-03-06 00:41:10 +0300

    rf:Resturcture-Restructred the container divs

--- js/script.js
+++ js/script.js
@@ -3,15 +3,24 @@ var pageTwo=document.getElementById('pagetwo');
 var akangenerator=document.getElementById('akangenerator');
 var pageOne=document.getElementById('pageOne');
 var pageTwo=document.getElementById('pagetwo');
+var pageThree=document.getElementById('pageThree');
+var fullPageTwo=document.getElementById('fullPageTwo');
 
 akangenerator.addEventListener('click',AkanEvent);
+pageTwo.addEventListener('submit',FormEvent);
 
+formEvent=(e)=>{
+  e.preventDefault();
+ 
 
-
+  
+}
 function AkanEvent(e){
   e.preventDefault();
  
   pageOne.style.display='none';
-  pageTwo.style.display='initial'
+  pageTwo.style.display='initial';
+  fullPageTwo.style.display='initial';
+
 
 }
