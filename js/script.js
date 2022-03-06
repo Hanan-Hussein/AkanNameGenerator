7828f87d93e1ab73a4d5e02d845505d1773312e8
Hanan-Hussein <hanhuss035@gmail.com>
2022-03-06 10:57:58 +0300

    ft:Alert-Outputted an alert when form value is blank

--- js/script.js
+++ js/script.js
@@ -1,35 +1,61 @@
-var pageOne=document.getElementById('pageOne');
-var pageTwo=document.getElementById('pagetwo');
-var akangenerator=document.getElementById('akangenerator');
-var pageOne=document.getElementById('pageOne');
-var pageTwo=document.getElementById('pagetwo');
-var pageThree=document.getElementById('pageThree');
-var fullPageTwo=document.getElementById('fullPageTwo');
-var back=document.getElementById('back');
+var pageOne = document.getElementById('pageOne');
+var pageTwo = document.getElementById('pagetwo');
+var akangenerator = document.getElementById('akangenerator');
+var pageOne = document.getElementById('pageOne');
+var pageTwo = document.getElementById('pagetwo');
+var pageThree = document.getElementById('pageThree');
+var fullPageTwo = document.getElementById('fullPageTwo');
+var back = document.getElementById('back');
+var exit = document.getElementById('exit');
+var dates = document.getElementById('dates')
 
-akangenerator.addEventListener('click',AkanEvent);
-pageTwo.addEventListener('submit',formEvent);
-back.addEventListener('click',backEvent);
+akangenerator.addEventListener('click', AkanEvent);
+pageTwo.addEventListener('submit', formEvent);
+back.addEventListener('click', backEvent);
+exit.addEventListener('click', exitEvent);
 
-function formEvent(e){
-    e.preventDefault();
-    pageOne.style.display='none';
-    pageTwo.style.display='none';
-    pageThree.style.display='initial'
-    
-  
-}
-function AkanEvent(e){
+
+function formEvent(e) {
   e.preventDefault();
- 
-  pageOne.style.display='none';
-  pageTwo.style.display='initial';
-  fullPageTwo.style.display='initial';
+
+  if (dates.value === "") {
+    alert("Please input a birth date");
+    return;
+  } else if (pageTwo.gender.value === "") {
+    alert("Please input a gender");
+    return;
+  }
+
+  pageOne.style.display = 'none';
+  pageTwo.style.display = 'none';
+  pageThree.style.display = 'initial'
+
 
 }
-function backEvent (e){
+var gender=()=>{
+  return pageTwo.gender.value;
+}
+
+function AkanEvent(e) {
   e.preventDefault();
-  pageThree.style.display='none';
-  pageTwo.style.display='initial';
-  fullPageTwo.style.display='initial';
+
+  pageOne.style.display = 'none';
+  pageTwo.style.display = 'initial';
+  fullPageTwo.style.display = 'initial';
+
+}
+
+function backEvent(e) {
+  e.preventDefault();
+  pageThree.style.display = 'none';
+  pageTwo.style.display = 'initial';
+  fullPageTwo.style.display = 'initial';
+}
+
+function exitEvent(e) {
+  e.preventDefault();
+  pageThree.style.display = 'none';
+  pageTwo.style.display = 'none';
+  fullPageTwo.style.display = 'none';
+  pageOne.style.display = 'initial';
 }
